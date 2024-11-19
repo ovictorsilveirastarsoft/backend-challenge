22a0ee4b5ca387bc73745456541602eba9c77563
ovictorsilveirastarsoft <vanhanguera@gmail.com>
2024-11-19 13:51:36 -0300

    refactor kafka

---
+++ src/kafka/consumer.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, OnApplicationShutdown } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ConsumerConfig, ConsumerSubscribeTopic, KafkaMessage } from 'kafkajs';
+import { IConsumer } from './consumer.interface';
+import { KafkajsConsumer } from './kafkajs.consumer';
+
+interface KafkajsConsumerOptions {
+  topic: ConsumerSubscribeTopic;
+  config: ConsumerConfig;
+  onMessage: (message: KafkaMessage) => Promise<void>;
+}
+
+@Injectable()
+export class ConsumerService implements OnApplicationShutdown {
+  private readonly consumers: IConsumer[] = [];
+
+  constructor(
+    private readonly configService: ConfigService,
+  ) {}
+
+  async consume({ topic, config, onMessage }: KafkajsConsumerOptions) {
+    const consumer = new KafkajsConsumer(
+      topic,
+      config,
+      this.configService.get('KAFKA_BROKER'),
+    );
+    await consumer.connect();
+    await consumer.consume(onMessage);
+    this.consumers.push(consumer);
+  }
+
+  async onApplicationShutdown() {
+    for (const consumer of this.consumers) {
+      await consumer.disconnect();
+    }
+  }
+}
--- src/kafka/kafka.module.ts
+++ src/kafka/kafka.module.ts
@@ -1,26 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { KafkaService } from './kafka.service';
-import { UserConsumer } from './consumers/user.consumer'; // Exemplo de consumer
+import { DatabaseModule } from 'database/database.module';
+import { ProducerService } from './producer.service';
+import { ConsumerService } from './consumer.service';
 
 @Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'KAFKA_SERVICE',
-        transport: Transport.KAFKA,
-        options: {
-          client: {
-            brokers: ['kafka:9092'], 
-          },
-          consumer: {
-            groupId: 'my-consumer-group',
-          },
-        },
-      },
-    ]),
-  ],
-  providers: [KafkaService, UserConsumer],
-  exports: [KafkaService],
+  imports: [DatabaseModule],
+  providers: [ ProducerService, ConsumerService],
+  exports: [ProducerService, ConsumerService],
 })
 export class KafkaModule {}
